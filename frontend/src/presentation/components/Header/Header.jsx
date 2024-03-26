5cce1a98b109e666759b9ef7814101f1553fba79
Johnny <jonathanbc2012@gmail.com>
2024-03-26 19:14:39 +0100

    feat: header created

---
+++ frontend/src/presentation/components/Header/Header.jsx
@@ -0,0 +1,47 @@
+import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMagnifyingGlass,
+  faBars,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
+
+export const Header = () => {
+  return (
+    <header>
+      <input type="checkbox" id="check" />
+      <nav>
+        <div className="icon">
+          <img
+            src="../../../../LogoDevDojo.png"
+            alt="DevDojo logo image"
+            className="logo"
+          />
+        </div>
+        <div className="search_box">
+          <span>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </span>
+          <input type="search" placeholder="Search here..." />
+        </div>
+        <ol>
+          <li>
+            <a href="#">Iniciar Sesión</a>
+          </li>
+          <li>
+            <a href="#">Regístrate</a>
+          </li>
+        </ol>
+        <label htmlFor="check" className="bar">
+          <span>
+            <FontAwesomeIcon icon={faBars} id="bars" />
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faTimes} id="times" />
+          </span>
+        </label>
+      </nav>
+      <section className="section__header"></section>
+    </header>
+  );
+};
