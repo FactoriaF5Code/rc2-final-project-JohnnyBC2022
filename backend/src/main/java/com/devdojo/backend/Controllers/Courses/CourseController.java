37644ce9da261ad641b77168c46c8c620bb3a6c2
Johnny <jonathanbc2012@gmail.com>
2024-04-04 21:13:00 +0200

    fix: CourseController fixed

--- backend/src/main/java/com/devdojo/backend/Controllers/Courses/CourseController.java
+++ backend/src/main/java/com/devdojo/backend/Controllers/Courses/CourseController.java
@@ -1,6 +1,8 @@
 package com.devdojo.backend.Controllers.Courses;
 
+import com.devdojo.backend.Persistence.Course;
 import com.devdojo.backend.Persistence.CourseRepository;
+import com.devdojo.backend.Services.CourseSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,16 +16,26 @@ import java.util.stream.Collectors;
 public class CourseController {
     private CourseSearchService courseSearch;
     private CourseRepository courseRepository;
-   public CourseController(@Autowired CourseSearchService courseSearchService){
+   public  CourseController(@Autowired CourseSearchService courseSearchService){
        this.courseSearch = courseSearchService;
    }
 
    @GetMapping
     public CourseSearchResponse searchCourse(@RequestParam(name = "q", required = true) String query){
-        var courseResults = courseSearch.searchCourse(query)
+        var courseResults = courseSearch.searchCourses(query)
                 .stream()
                 .map(this::responseFromCourse)
                 .collect(Collectors.toList());
-        return  new CourseSearchResponse(courseResults);
+        return new CourseSearchResponse(courseResults);
+    }
+
+    private CourseResponse responseFromCourse(Course course) {
+       return new CourseResponse(
+               course.getCourseId().toString(),
+               course.getImageUrl(),
+               course.getCourseName(),
+               course.getDescription(),
+               course.getPrice()
+       );
     }
 }
