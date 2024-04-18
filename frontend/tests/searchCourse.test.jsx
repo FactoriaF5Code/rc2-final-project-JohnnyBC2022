647e9befb2f2f16adb4aab6172e4152f23e2283c
Johnny <jonathanbc2012@gmail.com>
2024-04-18 19:08:15 +0200

    all test ok

--- frontend/tests/searchCourse.test.jsx
+++ frontend/tests/searchCourse.test.jsx
@@ -1,35 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
-import { beforeAll, expect } from "vitest";
+import { expect } from "vitest";
 import userEvent from "@testing-library/user-event";
 import App from "../src/App";
-import { setupServer } from "msw/node";
-import { HttpResponse, http } from "msw";
+import "./testServer"
 
-const handlers = [
-  http.get("http://localhost:8080/api/courses", ({ request }) => {
-    return HttpResponse.json([
-      {
-        id: "68ec1461-7914-4ac0-9600-1c6921cf078d",
-        imageUrl:
-          "https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        courseName: "Iniciación a HTML",
-        description:
-          "Aprende los fundamentos de HTML y cómo crear estructuras básicas para páginas web.",
-        price: "Gratis",
-      },
-    ]);
-  }),
-];
+test("Search view", ()=>{
+  render(<App />);
+  const searchBar = screen.getByPlaceholderText(/Buscar/);
+  expect(searchBar).toBeInTheDocument();
 
-const server = setupServer(...handlers);
+    expect(screen.queryByText(/Iniciación a Javascript/)).not.toBeInTheDocument();
 
-beforeAll(() => server.listen());
-afterAll(() => server.close());
+})
 
-test("we can search for a course", async () => {
+test("user can search for a course", async () => {
   render(<App />);
   const searchBar = screen.getByPlaceholderText(/Buscar/);
   await userEvent.type(searchBar, "HTML");
-  expect(await screen.findByText(/Iniciación a HTML/)).toBeInTheDocument();
+  const typeText = screen.findByText(/Iniciación a HTML/)
+  expect(await typeText).toBeInTheDocument();
 });
---
+++ frontend/tests/testServer.js
@@ -0,0 +1,27 @@
+import "@testing-library/jest-dom/vitest";
+import { beforeAll, afterAll } from "vitest";
+import { setupServer } from "msw/node";
+import { HttpResponse, http } from "msw";
+
+const handlers = [
+  http.get("http://localhost:8080/api/courses", () => {
+    return HttpResponse.json([
+      {
+        id: "68ec1461-7914-4ac0-9600-1c6921cf078d",
+        imageUrl:
+          "https://images.unsplash.com/photo-1542831371-29b0f74f9713?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        courseName: "Iniciación a HTML",
+        description:
+          "Aprende los fundamentos de HTML y cómo crear estructuras básicas para páginas web.",
+        price: "Gratis",
+      },
+    ]);
+  }),
+];
+
+const server = setupServer(...handlers);
+
+beforeAll(() => server.listen());
+afterAll(() => server.close());
+
+
